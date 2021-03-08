4f77406c72c02628c1b73a6ae015a4f92231307b
Kyle Kindt <k.kindt8@gmail.com>
2021-03-07 21:28:17 -0500

    commit tree ced56df8c635e5c6f0555b52402adce469b1b8f9

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+require "prime"
+
+def prime?(num)
+    n = 2
+  while n < num
+    return false if num % n == 0
+    n += 1
+  end
+  if num <= 1
+    false
+  
+else 
+    true
+end 
+end
+
